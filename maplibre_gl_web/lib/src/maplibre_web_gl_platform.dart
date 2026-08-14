41f6562e6b56efaa56ea0fd42d7fd069b78db272
Nico04 <34476051+Nico04@users.noreply.github.com>
2026-08-14 14:03:57 +0200

    added moveLayer()

--- maplibre_gl_web/lib/src/maplibre_web_gl_platform.dart
+++ maplibre_gl_web/lib/src/maplibre_web_gl_platform.dart
@@ -1197,6 +1197,11 @@ class MapLibreMapController extends MapLibrePlatform
   }
 
   @override
+  Future<void> moveLayer(String layerId, {String? belowLayerId}) async {
+    _map.moveLayer(layerId, belowLayerId);
+  }
+
+  @override
   Future<void> setFilter(String layerId, dynamic filter) async {
     _map.setFilter(layerId, filter);
   }
